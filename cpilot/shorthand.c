447251b0d17e35d62491f9fecac51c60d9e70d98
hoodielive <hoodielive@gmail.com>
2019-09-06 10:09:51 -0400

    shorthands

---
+++ cpilot/shorthand.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[])
+{
+    int a;
+    scanf("%d", &a);
+    a -= 1;
+    printf("The predecessor of the number is: %d\n", a);
+    return 0;
+}
