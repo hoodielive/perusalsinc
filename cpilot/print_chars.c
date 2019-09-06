a08118f561ebf8e78849bcab3add4ddada4dc1b5
hoodielive <hoodielive@gmail.com>
2019-09-06 14:52:00 -0400

    hello

--- cpilot/print_chars.c
+++ cpilot/print_chars.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char * argv[])
 {
-    char text[6] = { 'H', 'e', 'l', 'l', 'o', '!' };
+    char text[6] = { 'H', 'e', 'l', 'l', 'o', '!', '\0' };
 
     printf("%s", text);
     return 0;
